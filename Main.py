848f1e5c3034e648e41d8579935a9f8d7ddbd2bf
yuvraj420190 <108419015+yuvraj420190@users.noreply.github.com>
2022-10-12 09:32:12 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -2,6 +2,32 @@ from typing import List
 
 def merge_sort(data) -> None:
   # Write code here
+  if len(data)>1:
+        mid = len(data)//2
+        lefthalf = data[:mid]
+        righthalf = data[mid:]
+
+        merge_sort(lefthalf)
+        merge_sort(righthalf)
+        i=j=k=0       
+        while i < len(lefthalf) and j < len(righthalf):
+            if lefthalf[i] < righthalf[j]:
+                data[k]=lefthalf[i]
+                i=i+1
+            else:
+                data[k]=righthalf[j]
+                j=j+1
+            k=k+1
+
+        while i < len(lefthalf):
+            data[k]=lefthalf[i]
+            i=i+1
+            k=k+1
+
+        while j < len(righthalf):
+            data[k]=righthalf[j]
+            j=j+1
+            k=k+1
 
 
 # Do not change the following code
